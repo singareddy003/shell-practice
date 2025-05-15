72b8ce7d0af1cc36094dc7386c4df59ce0092654
singareddy <singareddy003@gmail.com>
2025-05-16 00:51:57 +0530

    hello world started

---
+++ conver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "ramu: hey krish, how are you"
+echo "krish: hello ramu, i am doing good and how you are? "
+echo "ramu: i am fine"
+echo "krish: thanks"
---
+++ vara.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "enter your pin no::"
+
+read PIN
+
+echo "Your no is : $PIN"
