72716bf29263c91a032cc89a9429b793a8e5c5ef
singareddy <singareddy003@gmail.com>
2025-05-17 08:55:24 +0530

    special vars

--- 6-conditions.sh
+++ 6-conditions.sh
@@ -3,7 +3,7 @@
 
 if [ $NUMBER -lt 10 ]
 then
-echo "given number $NUMBER is less than 10"
+ echo "given number $NUMBER is less than 10"
 else
-echo "given number is $NUMBER is not less than 10"
+ echo "given number is $NUMBER is not less than 10"
 fi
