cd6b0860c6421f24ad8a495e132e252050c7641d
singareddy <singareddy003@gmail.com>
2025-05-17 08:33:03 +0530

    special vars

--- 5-special-vars.sh
+++ 5-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo " all varables passed to script : $@"
 echo " number varabiles:$#"
+echo " script name: $0"
+echo " current directory: $PWD"
+echo " user running thiss cript: $USER"
+echo " home directory of the user : $HOME"
+echo " PID of the script : $$"
+sleep 10 $
+echo " pid of last command in background: $!
