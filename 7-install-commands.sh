3ea18b9da1661c6d805a92cef95e734e4f4c708b
singareddy <singareddy003@gmail.com>
2025-05-18 05:53:24 +0530

    special vars

--- 7-install-commands.sh
+++ 7-install-commands.sh
@@ -9,12 +9,41 @@ else
 echo "you are running with root access"
 fi
 
-dnf install mysql -y
+VALIDATE()
 
+dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0]
+then 
+echo "mysql is not installed .. goint o install
+dnf install mysql -y
 if [ $? -eq 0 ]
 
 then
 echo "installing MYSQL is sucess ..."
 else 
 echo "installing MYSQL is failure .."
+fi
+
+dnf list installed python3
+if [ $? ne 0 ]
+then
+dnf install python3 -y
+VALIDATE $?"python3"
+
+else
+echo "python is already installed nothing to do"
+
+fi
+
+dnf list installed nginx 
+if [ $? ne 0 ]
+then
+dnf install nginx -y
+VALIDATE $?"nginx"
+
+else
+echo "nignix is already installed nothing to do"
+
 fi
