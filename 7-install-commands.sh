cbb1535ad4378ddf12dd3124df5065dced438e43
singareddy <singareddy003@gmail.com>
2025-05-18 06:12:00 +0530

    special vars

--- 7-install-commands.sh
+++ 7-install-commands.sh
@@ -4,46 +4,53 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "ERROR:: please run with root access"
-exit 1
+exit 1 #give other than 0 upto 127
 else
 echo "you are running with root access"
 fi
 
-VALIDATE()
+VALIDATE(){
+    if [ $? -eq 0 ]
+then 
+echo "installing MySQL is .. sucess"
+else
 
-dnf install MYSQL -y
+echo "installing MySQL is .. failure"
+exit 1
+fi
+
+}
+
 dnf list installed mysql
 
 if [ $? -ne 0]
 then 
 echo "mysql is not installed .. goint o install
 dnf install mysql -y
-if [ $? -eq 0 ]
-
-then
-echo "installing MYSQL is sucess ..."
-else 
-echo "installing MYSQL is failure .."
+VALIDATE $? "MySQL"
+else
+echo "mysql is already installed .. nothing to do"
 fi
+
 
 dnf list installed python3
-if [ $? ne 0 ]
-then
+
+if [ $? -ne 0]
+then 
+echo "python is not installed .. goint o install
 dnf install python3 -y
-VALIDATE $?"python3"
-
+VALIDATE $? "python3"
 else
-echo "python is already installed nothing to do"
-
+echo "python is already installed .. nothing to do"
 fi
 
-dnf list installed nginx 
-if [ $? ne 0 ]
-then
+dnf list installed nginx
+
+if [ $? -ne 0]
+then 
+echo "nginx is not installed .. goint to install
 dnf install nginx -y
-VALIDATE $?"nginx"
-
+VALIDATE $? "nginx"
 else
-echo "nignix is already installed nothing to do"
-
+echo "nginx is already installed .. nothing to do"
 fi
