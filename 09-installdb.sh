dcd7baa7db84aea66b44a4b0ae44e85b4a304aa9
singareddy <singareddy003@gmail.com>
2025-05-18 07:32:34 +0530

    special vars

---
+++ 09-installdb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID= $(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "error : run this command with root access "
+exit 1
+dnf install mongodb-org -y 
+if [ $? -eq 0 ]
+then
+echo " install mongodb is sucess:"
+else 
+echo "install mngodb is failure:"
+fi
+
+
+
