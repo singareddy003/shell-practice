2d45f4675b2b967fb96e402af048010d27dffdae
singareddy <singareddy003@gmail.com>
2025-05-18 08:05:10 +0530

    special vars

--- 09-installdb.sh
+++ 09-installdb.sh
@@ -8,11 +8,10 @@ exit 1
 dnf install mongodb-org -y 
 if [ $? -eq 0 ]
 then
-echo "install mongodb is sucess"
+echo "Installing mongodb is .... SUCCESS"
 else 
-echo "install mngodb is failure"
+echo "Installing mngodb is.....FAILURE"
 exit 1
-
 fi
 
 
